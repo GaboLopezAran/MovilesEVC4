59639b3c719e55de1cb7fc9be8e1bd7f44de1ca8
Fernando Yanac T. <sm73885839@idat.edu.pe>
2024-08-21 23:30:49 -0500

    Se realizon el login  y el registro de Dueño

--- app/src/main/java/com/idat/proyectoevc4/servicio/WebService.kt
+++ app/src/main/java/com/idat/proyectoevc4/servicio/WebService.kt
@@ -3,6 +3,8 @@ package com.idat.proyectoevc4.servicio
 
 import com.google.gson.GsonBuilder
 import com.idat.proyectoevc4.modelo.Cita
+import com.idat.proyectoevc4.modelo.Dueno
+import com.idat.proyectoevc4.modelo.LoginRequest
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -14,13 +16,19 @@ import retrofit2.http.PUT
 import retrofit2.http.Path
 
 object AppConstantes{
-    const val base_url="http://192.168.18.24:8070"
+    const val base_url="http://192.168.1.7:8070"
 }
 
 interface WebService {
 
     @POST("/Cita/Ingresar")
     suspend fun ingresarCita(@Body cita: Cita): Response<Cita>
+
+    @POST("Dueno/Ingresar")
+    suspend fun ingresarDueno(@Body dueno: Dueno): Response<Dueno>
+
+    @POST("/api/login")
+    suspend fun login(@Body loginRequest: LoginRequest): Response<String>
 
 }
 
